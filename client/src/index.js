f4a81b4e83a19af1664ad3c6a949645314812022
Ben Herman <ben.h@onoapps.com>
2020-09-20 13:52:18 +0300

    update3

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import './index.scss'
 import BookStore from './BookStore'
 import { Provider } from 'react-redux'
 import { store } from '../src/Components/redux/index'
-import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <Provider store={store}>
